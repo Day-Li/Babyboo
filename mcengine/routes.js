82cba0a6bad355b0bf7dc86094eb49f9ee4e7d9b
dhruthikuram <157140647+dhruthikuram@users.noreply.github.com>
2024-01-20 12:21:34 +0530

    Middleware initial commit

---
+++ mcengine/routes.js
@@ -0,0 +1,16 @@
+'use strict'
+
+//Define all the routes in the server running on multichain cluster
+module.exports = function routes(app) {
+    //GET routes
+    app.get('/queryDataByKey', require('./data/query').fetchDataByKey);
+    //POST routes
+    app.post('/registerUser', require('./users/register').createUser);
+    app.post('/registerMilestone', require('./users/registerBigData').createBigUser);
+    app.post('/login', require('./users/login').login);
+    app.post('/fblogin', require('./users/fblogin').fblogin);
+    app.get('/',require('./common/middleware').checkToken, require('./users/login').index);
+
+}
+
+
